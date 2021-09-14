b576416e89a724bceebfedad0a42319b7e9f4602
shadrach akaade <akaadesm@gmail.com>
2021-09-14 12:15:11 -1000

    update bubble_sort code

---
+++ bubble_sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort(fig)
+    num = fig.length
+
+    loop do
+        sorted = false
+        (num-1).times do |a|
+            if fig[a] > fig[a + 1]
+                fig[a], fig[a + 1] = fig[a + 1], fig[a]
+                sorted = true
+    end
+end
+break if not sorted
+end 
+fig
+end
+
+val = [4,3,78,2,0,2]
+print bubble_sort val 
