84cb81af47bb471cf5d3e5b0d403516562b75472
shadrach akaade <akaadesm@gmail.com>
2021-09-17 11:28:21 -1000

    solve linter errors

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,16 +1,16 @@
 def bubble_sort_by(fig)
   num = fig.length
-    loop do
-  sorted = false
-      (num - 1).times do |a|
-  if (yield [fig[a], fig[a + 1]]).positive?
+  loop do
+    sorted = false
+    (num - 1).times do |a|
+      if (yield [fig[a], fig[a + 1]]).positive?
         fig[a], fig[a + 1] = fig[a + 1], fig[a]
         sorted = true
-  end
       end
-break if !sorted
     end
-fig
+    break if !sorted
+  end
+  fig
 end
 val = bubble_sort_by(%W[hi, hello, hey]) do |left, right|
   left.length - right.length
